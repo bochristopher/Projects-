f6fec5d49dd879a92b0719329149433d88d26294
Bo Redfearn <75061442+bochristopher@users.noreply.github.com>
2023-02-12 14:38:01 -0800

    updated code with comments

--- #state machine traversal.py
+++ #state machine traversal.py
@@ -3,56 +3,58 @@
 from random import randint
 import time
 
-State = type ("State",(object,),{})
+State = type ("State",(object,),{})# define State base class
 
-class LightOn(State):
-	def Execute(self):
-		print ("Light is ON")
+class LightOn(State): # define class for LightOn with State
+	def Execute(self): # define function for class variable.Execute()
+		print ("Light is ON") # print the action 
 
-class LightOff(State):
-	def Execute(self):
+class LightOff(State): # define class for LightOff with State
+	def Execute(self): # define function in class
 		print ("Light is OFF")
 
-class Transition(object):
-	def __init__(self,toState):
-		self.toState=toState
+class Transition(object):# define class with object name 
+	def __init__(self,toState):# define contructor class with 	
+		self.toState=toState #equal to itself
 
-	def Execute(self):
+	def Execute(self): #define method 
 		print ("Transitioning...")
 
 class SimpleFSM(object):
-	def __init__(self,char):
+	def __init__(self,char): # obstructor to construct/intialize the other variables  
 		self.char=char
 		self.states = {}
 		self.transitions = {}
 		self.curState = None
 		self.trans =None
 
-	def SetState(self, stateName):
+	def SetState(self, stateName):# used for defining states 
 		self.curState=self.states[stateName]
 
-	def Transisition(self,transName):
+	def Transisition(self,transName):#used for defining states
 		self.trans = self.transitions[transName]
 
 	def Execute(self):
-		if(self.trans):
-			self.trans.Execute()
-			self.SetState(self.trans.toState)
-			self.trans = None
-		self.curState.Execute()
+		if(self.trans):# if there is a transition stored within self.trans
+			self.trans.Execute() #execute transition 
+			self.SetState(self.trans.toState)# set current state to current state to 
+			self.trans = None # reset transition state to None
+		self.curState.Execute() # execute current state 
 
-class Char(object):
+class Char(object): #instance of FSM machine above 
 	def __init__(self):
 		self.FSM=SimpleFSM(self)
 		self.LightOn = True
 
 
 if __name__ == "__main__":
-	light=Char()
+	light=Char() # instance of the character 
 
-	light.FSM.states["On"]=LightOn()
+	light.FSM.states["On"]=LightOn() # created instance of the LightOn state 
+	#stored the state within the state dictionary inside the Finite State Machine 
 	light.FSM.states["Off"]=LightOff()
-	light.FSM.transitions["toOn"]=Transition("On")
+	light.FSM.transitions["toOn"]=Transition("On")# created an instance of transition
+	#stored it within the dicitionary of transitions 
 	light.FSM.transitions["toOff"]=Transition("Off")
 	
 	light.FSM.SetState("On")
@@ -60,9 +62,9 @@ if __name__ == "__main__":
 	...
 
 	for i in range(20):
-		startTime=time.perf_counter()
+		startTime=time.perf_counter() # not clock 
 		timeInterval=1
-		while (startTime +timeInterval>time.perf_counter()):
+		while (startTime +timeInterval>time.perf_counter()):#not clock
 			pass
 		if(randint(0,2)):
 			if(light.LightOn):
